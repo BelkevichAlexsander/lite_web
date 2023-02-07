066ed74ad8196aa0bde56525870d2dad0db8ca48
BelkevichAlexsander <jasonstyler58@gmail.com>
2023-02-07 22:18:43 +0300

    create models software

--- lite_web/lite/models.py
+++ lite_web/lite/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Version(models.Model):
+    """
+    :param name => название программного обеспечения
+    :param first => первое число версии
+    :param second => второе число версии
+    :param third => третье число версии
+    """
+    name = models.CharField(max_length=64, unique=True)
+    first = models.PositiveIntegerField(default=0)
+    second = models.PositiveIntegerField(default=0)
+    third = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        """
+        функция вывода названия и версии программного обеспечения
+        :return: строку на подобии json формата
+        """
+        return f'software: {self.name}\n' \
+               f'version: {self.first}.{self.second}.{self.third}'
+
