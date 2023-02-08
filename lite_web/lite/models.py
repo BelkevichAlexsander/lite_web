bde7f40a6045bacc313e8955f80445217f5241e4
BelkevichAlexsander <jasonstyler58@gmail.com>
2023-02-08 17:33:22 +0300

    сделал версию без цифр

--- lite_web/lite/models.py
+++ lite_web/lite/models.py
@@ -4,20 +4,14 @@ from django.db import models
 class Version(models.Model):
     """
     :param name => название программного обеспечения
-    :param first => первое число версии
-    :param second => второе число версии
-    :param third => третье число версии
+    :param version => версия программного обеспечения
     """
     name = models.CharField(max_length=64, unique=True)
-    first = models.PositiveIntegerField(default=0)
-    second = models.PositiveIntegerField(default=0)
-    third = models.PositiveIntegerField(default=0)
+    version = models.CharField(max_length=32)
 
     def __str__(self):
         """
-        функция вывода названия и версии программного обеспечения
+        Функция вывода названия и версии программного обеспечения
         :return: строку на подобии json формата
         """
-        return f'software: {self.name}\n' \
-               f'version: {self.first}.{self.second}.{self.third}'
-
+        return f'software: {self.name}\nversion: {self.version}'
